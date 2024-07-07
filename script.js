f30d2891a54a727d887a4cb21394762908275933
babitamittal <babitamittal14@hotmail.com>
2024-07-07 23:14:08 +0530

    subtraction feature added

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ add=num1+num2;
 document.getElementById("result").value=add;
 }
 
+// subtraction feature added by subdev1
+
+function subt()
+{num1=Number(document.getElementById("num1").value);
+num2=Number(document.getElementById("num2").value);
+subt=num1-num2;
+document.getElementById("result").value=subt;
+}
+
+
 function clear1()
 {document.getElementById("result").value="";
 document.getElementById("num1").value="";
